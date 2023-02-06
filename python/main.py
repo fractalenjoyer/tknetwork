2a5147968bcd66a126e2cb726537e319611f43a0
fractalenjoyer <92572174+fractalenjoyer@users.noreply.github.com>
2023-02-06 15:21:54 +0100

    started working on rust sockets

--- python/main.py
+++ python/main.py
@@ -1,13 +1,9 @@
-import tknetwork
-# from concurrent.futures import ThreadPoolExecutor
+from tknetwork import Network
 
-def on_message(message):
-    print(message)
+net = Network()
 
-# executor = ThreadPoolExecutor(max_workers=1)
-# executor.submit(tknetwork.start_server, ("127.0.0.1:12345", on_message))
+@net.events.bind("test")
+def hello_name(name):
+    print(f"Hello, {name}")
 
-# threading.Thread(target=tknetwork.start_server, args=(), daemon=True)
-tknetwork.udp_server("127.0.0.1:12345", on_message)
-while (i:=input()) != "exit":
-    tknetwork.udp_send(i, "127.0.0.1:12345")
+net.emit("test", "world")
--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,168 @@
 use pyo3::prelude::*;
-use pyo3::types::PyTuple;
+use pyo3::types::{PyDict, PyTuple};
 include!(concat!(env!("OUT_DIR"), "/module.rs"));
 
-use std::{net::{ TcpListener, TcpStream, UdpSocket }, io::{ Read, Write }, thread::JoinHandle, str};
+use std::collections::HashMap;
+use std::net::{TcpListener, TcpStream, UdpSocket};
+use std::sync::{Arc, Mutex};
 use std::thread;
 
-#[pyfunction]
-fn add_one(x: i32) -> PyResult<i32> {
-    Ok(x + 1)
+#[pyclass]
+struct Event {
+    callback: Option<Py<PyAny>>,
 }
 
-#[pyfunction]
-fn start_server(address: String, callback: Py<PyAny>) -> PyResult<()> {
-    
-    let handle: JoinHandle<Result<(), PyErr>> = thread::spawn( move || {
-        println!("Starting server");
-        let listener = TcpListener::bind(address)?;
-        for connection in listener.incoming() {
-            let mut stream = match connection {
-                Ok(stream) => stream,
-                Err(e) => {
-                    println!("Error: {}", e);
-                    continue;
-                }
-            };
-            let mut buffer = String::new();
-            stream.read_to_string(&mut buffer)?;
+#[pymethods]
+impl Event {
+    fn __call__(&mut self, py: Python, func: Py<PyAny>) -> PyResult<Py<PyAny>> {
+        self.callback = Some(func.clone_ref(py));
+        Ok(func)
+    }
 
-            Python::with_gil(|py| {
-                let args = PyTuple::new(py, &[buffer]);
-                callback.call1(py, args).expect("Error calling callback");
-            })
-
+    fn call(&self, py: Python, args: &PyTuple, kwargs: Option<&PyDict>) -> PyResult<()> {
+        if let Some(callback) = &self.callback {
+            callback.call(py, args, kwargs)?;
         }
         Ok(())
-    });
-
-    Ok(())  
+    }
 }
 
-#[pyfunction]
-fn send_message(message: String, address: String) -> PyResult<()> {
-    let mut stream = TcpStream::connect(address)?;
-    stream.write(message.as_bytes())?;
-    Ok(())
+#[pyclass]
+struct EventManager {
+    events: HashMap<String, Py<Event>>,
 }
 
-#[pyfunction]
-fn udp_server(address: String, callback: Py<PyAny>) -> PyResult<()> {
-    thread::spawn(move || {
-        println!("Starting server");
-        let socket = UdpSocket::bind(address).expect("Error binding socket");
-        loop {
-            let mut buf = [0; 30];
-            match socket.recv_from(&mut buf) {
-                Ok(e) => {
-                    Python::with_gil(|py| {
-                        let args = PyTuple::new(py, &[String::from(str::from_utf8(&buf[..e.0]).unwrap())]);
-                        callback.call1(py, args).expect("Error calling callback");
-                    })
-                },
-                Err(e) => {
-                    println!("Error: {}", e);
-                    continue;
-                }
-            }
+#[pymethods]
+impl EventManager {
+    #[new]
+    fn new() -> Self {
+        Self {
+            events: HashMap::new(),
         }
-    });
-    Ok(())
+    }
+
+    fn bind(&mut self, py: Python, name: &str) -> PyResult<Py<Event>> {
+        let event = Py::new(py, Event { callback: None })?;
+        self.events.insert(name.to_string(), event.clone_ref(py));
+        Ok(event)
+    }
+
+    #[pyo3(signature = (name, *args, **kwargs))]
+    fn trigger(
+        &self,
+        py: Python,
+        name: &str,
+        args: &PyTuple,
+        kwargs: Option<&PyDict>,
+    ) -> PyResult<()> {
+        if let Some(event) = self.events.get(name) {
+            event.borrow(py).call(py, args, kwargs)?;
+        }
+        Ok(())
+    }
 }
 
-#[pyfunction]
-fn udp_send(message: String, address: String) -> PyResult<()> {
-    let socket = UdpSocket::bind("127.0.0.1:24387").expect("Error binding socket");
-    socket.send_to(message.as_bytes(), address)?;
-    Ok(())
+#[pyclass]
+struct Peer {
+    #[pyo3(get)]
+    events: Py<EventManager>,
+    global_events: Py<EventManager>,
+    socket: TcpStream,
+}
+
+#[pymethods]
+impl Peer {
+    fn connect(&self, py: Python, address: &str) -> PyResult<()> {
+        self.global_events
+            .borrow(py)
+            .trigger(py, "connect", PyTuple::new(py, &[address]), None)?;
+        Ok(())
+    }
+}
+
+#[pyclass]
+struct Network {
+    #[pyo3(get)]
+    events: Py<EventManager>,
+    peers: Arc<Mutex<Vec<Py<Peer>>>>,
+    address: String,
+}
+
+#[pymethods]
+impl Network {
+    #[new]
+    fn new(py: Python, address: &str) -> PyResult<Self> {
+        Ok(Self {
+            address: address.to_string(),
+            events: Py::new(py, EventManager::new())?,
+            peers: Arc::new(Mutex::new(Vec::new())),
+        })
+    }
+
+    fn connect(&mut self, address: &str) -> PyResult<()> {
+        let socket = UdpSocket::bind("127.0.0.1:7337")?;
+        socket.send_to(&[0], address)?;
+        Ok(())
+    }
+
+    #[pyo3(signature = (name, *args, **kwargs))]
+    fn emit(
+        &self,
+        py: Python,
+        name: &str,
+        args: &PyTuple,
+        kwargs: Option<&PyDict>,
+    ) -> PyResult<()> {
+        self.events.borrow(py).trigger(py, name, args, kwargs)?;
+        Ok(())
+    }
+
+    fn tcp_server(&self, py: Python) -> PyResult<()> {
+        let listener = TcpListener::bind(&self.address)?;
+        let peers = self.peers.clone();
+        let events = self.events.clone_ref(py);
+
+        thread::spawn(move || {
+            for stream in listener.incoming() {
+                let connection = match stream {
+                    Ok(stream) => stream,
+                    Err(e) => {
+                        println!("Error: {}", e);
+                        continue;
+                    }
+                };
+
+                Python::with_gil(|py| {
+                    let peer = match Py::new(
+                        py,
+                        Peer {
+                            events: Py::new(py, EventManager::new()).unwrap(),
+                            global_events: events.clone_ref(py),
+                            socket: connection,
+                        },
+                    ) {
+                        Ok(peer) => peer,
+                        Err(e) => {
+                            println!("Error: {}", e);
+                            return;
+                        }
+                    };
+                    match peers.lock() {
+                        Ok(mut peers) => peers.push(peer),
+                        Err(e) => {
+                            println!("Error: {}", e);
+                            return;
+                        }
+                    };
+                });
+            }
+        });
+
+        Ok(())
+    }
+
+    #[getter]
+    fn peers(&self) -> PyResult<Vec<Py<Peer>>> {
+        Ok(self.peers.lock().unwrap().clone())
+    }
 }
